7e6c8fc543a02a79115ea4fa72f8b67d55216105
luciana-s-oliveira <luciana.g.silva.ls@gmail.com>
2024-05-02 17:47:55 -0300

    Meu Projeto Conta Banco

--- contabanco/ContaBanco/src/ContaTerminal.java
+++ contabanco/ContaBanco/src/ContaTerminal.java
@@ -1,8 +1,5 @@
-public class TiposVariaveis {
+public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-
-        double salarioMinimo = 2500 ;
-
     }
 }
